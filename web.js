ab04e557f7c2dffa9cd5884130bb0bd62d5d718b
Caique Lira <caiqueportolira@gmail.com>
2013-07-08 19:25:19 +0000

    buffer

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
     buffer = fs.readFileSync('index.html');
-    response.send(buffer.toString("utf-8");
+    response.send(buffer.toString("utf-8"));
   }
 });
 
