97ffbb4e0ba7c1feb7553dba26fe62e10c2ab066
Caique Lira <caiqueportolira@gmail.com>
2013-08-06 23:15:15 +0000

    don't know what happened

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
 var fs = require('fs');
-
 app.get('/', function(request, response) {
-    //response.send('Hello World 2!');
     buffer = fs.readFileSync('index.html');
-    response.send(buffer.toString("utf-8"));
+    response.send(buffer.toString('utf-8'));
 });
-
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log('Listening on ' + port);
 });
